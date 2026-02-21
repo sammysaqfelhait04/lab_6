2939965907dbc36cb0901a228333901864d17923
sammy saqfelhait <ssaqfelhait@student.cscc.edu>
2026-02-20 23:29:01 -0500

    Updated Lab 6 - added final version

---
+++ Lab6_ssaqfelhait.py
@@ -0,0 +1,42 @@
+"""
+ Lab 6  User Login System
+sammy Saqfelhait
+This program   for login system using a dictionary to store usernames and passwords. 
+It  will checks if the username exists and allows the user up to three password attempts before locking the account.
+Date: 02/20/2026"""
+from typing import Dict
+
+def main() -> None:
+    users: Dict[str, str] = {
+        "sam": "sam123",
+        "joe": "pass123",
+        "adam": "Passw0rd1",
+        "sara": "sara54321",}
+    username: str = input("Enter username")
+
+    if username not in users:
+        print("\nUser not found. Exiting.")
+        return
+
+    attempts: int = 0
+    max_attempts: int = 3
+
+    while attempts < max_attempts:
+        password: str = input("Enter password: ")
+
+        if password == users[username]:
+            if username == "guest":
+                security_level: str = "Guest access"
+            else:
+                security_level = "Security Level 1"
+
+            print(f"\nWelcome, {username}. You have {security_level}.")
+            return
+        else:
+            attempts += 1
+            if attempts < max_attempts:
+                print("Access Denied. Try again.")
+            else:
+                print("\nToo many failed attempts. Account locked.")
+if __name__ == "__main__":
+    main()
